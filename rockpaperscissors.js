3da438aa85e09e8761773413000a1c45f481f652
JHamzat <40691596+JHamzat@users.noreply.github.com>
2024-12-01 19:47:34 +0000

    Add computer choice function, generating a random response of rock paper or scissors

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,1 +1,14 @@
 console.log("Hello World!")
+
+// function that randomly returns rock, paper or scissors, the computer
+function getComputerChoice() {
+    //random number is generated and stored in variable 'random'
+    let random = Math.random()*10
+    //if statement selects return value based on value of random
+    if (random <= 3.33) {
+        return "rock"
+    } else if (random >= 6.66) {
+        return "paper"
+    }
+    return "scissors"
+}
